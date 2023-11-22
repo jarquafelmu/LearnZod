4651778fb2401711eeaf173d29cdbde66060b371
Sam Terrazas <thechaoselement@gmail.com>
2023-11-21 19:30:09 -0700

    Optional properties

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { z } from "zod";
 
 const UserSchema = z.object({
   username: z.string(),
+  age: z.number().optional(),
+  birthday: z.date().optional(),
+  isProgrammer: z.boolean().optional(),
 });
 
 // with infer we can get the type of the schema
@@ -11,4 +14,4 @@ const user: User = { username: "WDS" };
 
 // safe parse returns an object with a success property which can be used to check if the parsing was successful
 // useful for validating a form
-console.log(UserSchema.safeParse(user));
+console.log(UserSchema.safeParse(user).success);
