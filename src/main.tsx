94af33d807c547fdfb5d1c6917dc019e811c5f67
Sam Terrazas <thechaoselement@gmail.com>
2023-11-21 19:27:46 -0700

    Zod basics

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,12 @@
 import { z } from "zod";
+
+const UserSchema = z.object({
+  username: z.string(),
+});
+
+// with infer we can get the type of the schema
+type User = z.infer<typeof UserSchema>;
+
+const user: User = { username: "WDS" };
+
+console.log(UserSchema.safeParse(user));
