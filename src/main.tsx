47ca9b0156174c4deeb582564446bd9e9c6dec1b
Sam Terrazas <thechaoselement@gmail.com>
2023-11-21 19:28:36 -0700

    more basic zod

--- src/main.tsx
+++ src/main.tsx
@@ -9,4 +9,6 @@ type User = z.infer<typeof UserSchema>;
 
 const user: User = { username: "WDS" };
 
+// safe parse returns an object with a success property which can be used to check if the parsing was successful
+// useful for validating a form
 console.log(UserSchema.safeParse(user));
